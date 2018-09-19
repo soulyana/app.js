752de500f6a64232b1b4daddc14d76d0d93e42c0
soulyana <soulakew@umd.edu>
2018-09-19 11:54:29 -0400

    used ./node_modules/.bin/eslint app.js --fix  to meet airbnb style

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// Pacakges in use 
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug');
-var morgan = require('morgan');
-var path = require('path');
+// Pacakges in use
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/'))) //setting up static directory & locatin
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/popper/dist')))
+app.use(express.static(path.join(__dirname, '/public/'))); // setting up static directory & locatin
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper/dist')));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/views/', '/index.html')); //location of current exutable
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/', '/index.html')); // location of current exutable
 });
 
-app.listen(3000, function() {
-    debug(`listening on port ${chalk.green('3000')}`); //string templates
-}); 
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`); // string templates
+});
