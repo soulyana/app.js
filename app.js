b36c00af1d79e3eea4e06188dfacf4f51019397d
soulyana <soulakew@umd.edu>
2018-09-19 11:00:35 -0400

    added gittignore, setting up tooling: NPM scripts, ESLint

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Pacakges in use 
+// Pacakges in use 
 var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug');
@@ -9,9 +9,13 @@ var app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/'))) //setting up static directory & locatin
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper/dist')))
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/views/index.html')); //location of current exutable
+    res.sendFile(path.join(__dirname, '/views/', '/index.html')); //location of current exutable
 });
 
 app.listen(3000, function() {
