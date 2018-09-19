b3ad45ddddb41d3d7d5f323fd264e9a003179ebd
soulyana <soulakew@umd.edu>
2018-09-19 09:15:02 -0400

    created css and js folders in public folder

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var path = require('path');
 var app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/'))) //setting up static directory & locatin
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html')); //location of current exutable
