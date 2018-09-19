0050073fe7844aad38337d07f97592cc8272c22f
soulyana <soulakew@umd.edu>
2018-09-19 13:59:23 -0400

    setup nodemon

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/'))); // setting up static directory & locatin
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper/')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/popper/dist')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/', '/index.html')); // location of current exutable
 });
 
-app.listen(3000, () => {
-  debug(`listening on port ${chalk.green('3000')}`); // string templates
+app.listen(port, () => {
+  debug(`listening at port ${chalk.green('port')}`); // string templates
 });
