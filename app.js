4b0dc7d6dfe9f062ad1e40f7412fab9a432b628d
soulyana <soulakew@umd.edu>
2018-09-21 12:03:33 -0400

    setup routing for books tab

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/'))); // setting up static directory & locatin
@@ -17,6 +18,17 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+  .get((req, res) => {
+    res.send('hello books');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
   res.render(
     'index',
