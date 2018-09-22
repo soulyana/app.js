fc721e0514dd05616bbcdc990b31c61df43dbc8b
soulyana <soulakew@umd.edu>
2018-09-21 21:09:25 -0400

    added middleware and solved sql connection error, solved bootsrap issue

--- app.js
+++ app.js
@@ -4,30 +4,20 @@ const chalk = require('chalk');
 const debug = require('debug');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const config = {
-  user: 'library',
-  password: 'soullibrary1!',
-  server: 'soulpslibrary.database.windows.net',
-  database: 'PSLibrary',
 
-  options: {
-    encrypt: true // Use this if you're on Windows Azure
-  }
-};
-
-sql.connect(config).catch(err => debug(err));
-
+// app.use((req, res, next) => {
+//   debug('my middleware');
+// });
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/'))); // setting up static directory & locatin
-app.use('/js', express.static(path.join(__dirname, '/node_modules/popper')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+// app.use('/js', express.static(path.join(__dirname, '/node_modules/popper')));
+// app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery')));
+// app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+// app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,6 +3,18 @@ const sql = require('mssql');
 const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
+const config = {
+  user: 'soulibrary',
+  password: 'soullibrary1!',
+  server: 'soulpslibrary.database.windows.net',
+  database: 'PSLibrary',
+
+  options: {
+    encrypt: true // Use this if you're on Windows Azure
+  }
+};
+
+sql.connect(config).catch(err => debug(err));
 
 function router(nav) {
   const books = [
@@ -62,21 +74,25 @@ function router(nav) {
     });
 
   bookRouter.route('/:id')
-    .get((req, res) => {
+    .all((req, res, next) => {
       (async function query() {
         const { id } = req.params;
         const request = new sql.Request();
         const { recordset } = await request.input('id', sql.Int, id)
           .query('select * from books where id = @id');
-        res.render(
-          'bookView',
-          {
-            nav,
-            title: 'Library',
-            book: recordset[id]
-          }
-        );
+        [req.book] = recordset;
+        next();
       }());
+    })
+    .get((req, res) => {
+      res.render(
+        'bookView',
+        {
+          nav,
+          title: 'Library',
+          book: recordset[0]
+        }
+      );
     });
   return bookRouter;
 }
