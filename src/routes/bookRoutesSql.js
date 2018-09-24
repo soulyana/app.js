4622dd5b6b0add8040e2f034c7d8fa615397e0ee
soulyana <soulakew@umd.edu>
2018-09-24 16:10:44 -0400

    setup selectMany in bookRoutes using MongoDB

--- src/routes/bookRoutesSql.js
+++ src/routes/bookRoutesSql.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
 const sql = require('mssql');
 const debug = require('debug')('app:bookRoutes');
 
@@ -58,33 +57,19 @@ function router(nav) {
   ];
   bookRouter.route('/')
     .get((req, res) => {
-      const url = 'mongodb://localhost:27017';
-      const dbName = 'libraryApp';
+      (async function query() {
+        const request = new sql.Request();
 
-      (async function mongo() {
-        let client;
-        try {
-          client = await MongoClient.connect(url);
-          debug('Connected correctly to server');
-
-          const db = client.db(dbName);
-
-          const col = await db.collection('books');
-
-          const books = await col.find().toArray();
-          
-          res.render(
-            'bookListView',
-            {
-              nav,
-              title: 'Library',
-              books
-            }
-          );
-        } catch (err) {
-          debug(err.stack);
-        }
-        client.close();
+        const { recordset } = await request.query('select * from books');
+        // debug(result);
+        res.render(
+          'bookListView',
+          {
+            nav,
+            title: 'Library',
+            books: recordset
+          }
+        );
       }());
     });
 
