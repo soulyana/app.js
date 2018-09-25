973ade958021e60d5e7e7d7664b051906eeb108a
David Turner <dturner@Davids-MacBook-Pro.local>
2018-09-25 14:17:41 -0400

    setup local strategy for user authentication

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+require('./strategies/local.strategy');
 
 module.exports = function passportConfig(app) {
     app.use(passport.initialize());
@@ -14,5 +15,5 @@ module.exports = function passportConfig(app) {
         done(null, user);
     });
 
-    require('./strategies/local.strategy');
+    
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,18 @@ const authRouter = express.Router();
 
 function router() {
     authRouter.route('/signUp')
-    .post((req, res) => {
-        debug(req.body);
-        res.json(req.body);
-    });
-return authRouter;
+        .post((req, res) => {
+            debug(req.body);
+            //create user
+            req.login(req.body, () => {
+                res.redirect('/auth/profile');
+            });
+        });
+    authRouter.route('/profile')
+        .get((req, res) => {
+            res.json(req.user);
+        });
+    return authRouter;
 }
 
 module.exports = router;
