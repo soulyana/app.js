3176b78809e1e874b3bd8ef3bf92951be85515fc
David Turner <dturner@Davids-MacBook-Pro.local>
2018-09-25 14:00:25 -0400

    created signup page, auth routes and passport for user authentication

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const chalk = require('chalk');
 const debug = require('debug');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +17,12 @@ const port = process.env.PORT || 3000;
 //   debug('my middleware');
 // });
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport.js')(app);
 app.use(express.static(path.join(__dirname, '/public/'))); // setting up static directory & locatin
 // app.use('/js', express.static(path.join(__dirname, '/node_modules/popper')));
 // app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery')));
@@ -28,9 +38,12 @@ const nav = [
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
+
 app.get('/', (req, res) => {
   res.render(
     'index',
