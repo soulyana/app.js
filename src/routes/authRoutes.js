40c0a3d176c7c1a75a3418fa2560a17192356ed6
David Turner <dturner@Davids-MacBook-Pro.local>
2018-09-25 17:13:06 -0400

    created signIn form page and routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
+const passport = require('passport');
 
 const authRouter = express.Router();
 
-function router() {
+function router(nav) {
     authRouter.route('/signUp')
         .post((req, res) => {
             const { username, password } = req.body;
@@ -23,16 +24,26 @@ function router() {
                     const user = { username, password };
                     const results = await col.insertOne(user);
                     debug(results);
-                     //create user
+                    //create user
                     req.login(results.ops[0], () => {
                         res.redirect('/auth/profile');
                     });
                 } catch (err) {
-                    debug(err);    
+                    debug(err);
                 }
-            }())
-            debug(req.body);
+            }());
         });
+    authRouter.route('/signIn')
+        .get((req, res) => {
+            res.render('signIn', {
+                nav,
+                title: 'Sign In'
+            });
+        })
+        .post(passport.authenticate('local', {
+        successRedirect: '/auth/profile',
+        failureRedirect: '/'
+    }));
     authRouter.route('/profile')
         .get((req, res) => {
             res.json(req.user);
